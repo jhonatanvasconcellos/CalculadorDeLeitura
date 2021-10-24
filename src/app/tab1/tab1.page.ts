1f4e0d5404ab36d427fa3861da7ea33d6d652442
Jhonatan Salgadoo <jhonatanvasconcellos89@gmail.com>
2021-10-24 10:48:09 -0300

    primeiro comit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -7,6 +7,44 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor() { }
 
+  quantPaginas;
+  cronometro;
+  tempo;
+  cronometroIniciado = false;
+  resultado = null;
+
+  start() {
+    if (this.quantPaginas > 0 && this.quantPaginas!= null){
+      this.cronometroIniciado = true;
+    this.tempo = 0;
+    this.cronometro = setInterval(() => {
+      this.tempo = this.tempo + 1;
+      //console.log(this.tempo);
+    }, 1000);
+  }else{
+    alert("informe uma quantidade de páginas válida!");
+  }
+  }
+
+
+  stop() {
+    this.cronometroIniciado = false;
+    clearInterval(this.cronometro);
+    this.mostrarResultado();
+  }
+
+mostrarResultado(){
+    this.tempo = this.tempo * this.quantPaginas;
+    this.resultado = new Date(this.tempo * 1000).toISOString().substr(11, 8);
+
+}
+
+reiniciar(){
+  this.quantPaginas = null;
+  this.tempo = 0;
+  this.cronometroIniciado = false;
+  this.resultado = null;
+}
 }
